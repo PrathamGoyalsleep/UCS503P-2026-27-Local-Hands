2e70ea89eed71a4a8232fafd41bc9fdb1ae6a567
KeshavGoyal-dot <goyalkeshav032@gmail.com>
2026-08-20 10:39:01 +0530

    Adding Frontend basic structure

---
+++ code/frontend/client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>ServeConnect</h2>
+      </div>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ code/frontend/client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to ServeConnect</h1>
+      <p>Find trusted local service workers.</p>
+    </div>
+  );
+}
+
+export default Home;
